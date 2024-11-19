25d1338db9f7460c18fee9ff6278bb9df02f2018
Puppper <58965954+Pupper0601@users.noreply.github.com>
2024-11-19 21:37:23 +0800

    1.修复路径获取错误问题

--- tools/paths.py
+++ tools/paths.py
@@ -9,15 +9,18 @@ from pathlib import Path
 # 定义全局变量
 PROJECT_PATH = None
 
+
 def project_path() -> str:
     global PROJECT_PATH
     if PROJECT_PATH is None:
         # 使用 sys.argv[0] 获取可执行文件路径
-        PROJECT_PATH = str(Path(sys.argv[0]).parent)
-    return PROJECT_PATH
+        PROJECT_PATH = str(Path(sys.argv[0]).parent).split("CuteAide")[0] + "CuteAide"
+    return PROJECT_PATH.replace("\\", "/")
 
-def path_conn(file_name: str='') -> str:
+
+def path_conn(file_name: str = '') -> str:
     return str(project_path() + file_name)
+
 
 if __name__ == '__main__':
     print(project_path())
