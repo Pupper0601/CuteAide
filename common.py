a605a6f1f522a1b987ed42bca4c32b726f77b7de
Puppper <58965954+Pupper0601@users.noreply.github.com>
2024-11-28 13:12:22 +0800

    0.40

--- common.py
+++ common.py
@@ -3,16 +3,15 @@
 # @Author : Pupper
 # @Email  : pupper.cheng@gmail.com
 
-version = "v0.39"
+version = "v0.40"
 
 notice = """
 登录成功! \n
     欢迎使用 CuteAide 应用程序。\n\n
     
 更新说明: \n
-    1. 修复账户无法保存的bug;\n
-    2. 修复长按多次触发识别的bug;\n
-    3. 优化压枪状态识别效率;\n\n
+    1. 更换武器名称原图;\n
+    2. 修复压枪数据获取失败的问题;\n\n
 
 公告：\n
     1. 欢迎加入 QQ群, 后续更新优先在群内发布.\n
